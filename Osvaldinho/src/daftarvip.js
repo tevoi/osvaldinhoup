90e5c3d7034f7b80c5a7e77fb66cdd395979d4b9
tevoi <90343960+tevoi@users.noreply.github.com>
2021-09-08 19:02:50 +0000

    Add files via upload

---
+++ Osvaldinho/src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/554792091566 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO TEVO :*
+_https://chat.whatsapp.com/IrKqmFSUsUA1NUPS7D52l_ `
+}
+exports.daftarvip = daftarvip
